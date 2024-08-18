99ce4ca8e9b6eefedc8bbc421af7b7658ec03a65
masum184e <masum184e@gmail.com>
2024-08-18 10:24:08 +0600

    training script added

--- nlp/Chatbot_Coffeshop/train.py
+++ nlp/Chatbot_Coffeshop/train.py
@@ -4,6 +4,10 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import numpy as np
 import json
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from sklearn.preprocessing import LabelEncoder
+import pickle
 
 nltk.download('punkt_tab')
 nltk.download('stopwords')
@@ -53,3 +57,25 @@ for (tokens, tag) in tokens_tag:
 
 X_train=np.array(X_train)
 y_train=np.array(y_train)
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(X_train[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(tags), activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=200, batch_size=8, verbose=1)
+model.save("chatbot_model.keras")
+
+with open('words.pkl', 'wb') as f:
+    pickle.dump(words, f)
+
+with open('tags.pkl', 'wb') as f:
+    pickle.dump(tags, f)
+
+print("Model training complete!")
